794fdd83accd7ff283fa582e3d47254596ea0cc4
Mel <mel.k.vangelova@gmail.com>
2023-01-09 17:49:55 -0500

    add post admin

--- mel_blog/blog/models.py
+++ mel_blog/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -9,11 +10,16 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    statistics = models.CharField(
+    status = models.CharField(
         max_length=2,
         choices=Status.choices,
         default=Status.DRAFT)
