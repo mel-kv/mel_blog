197c610631dd339156639308155e5b44155ac50e
Mel <mel.k.vangelova@gmail.com>
2023-02-01 12:56:09 -0500

    switch to PostgreSQL

--- mel_blog/settings.py
+++ mel_blog/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
+
 
 
 ]
@@ -57,10 +59,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mel_blog.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
