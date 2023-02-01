d91d35d0e9372dfc87c9a3c39c382c72119e30d0
Mel <mel.k.vangelova@gmail.com>
2023-02-01 13:12:50 -0500

    add search

--- mel_blog/blog/views.py
+++ mel_blog/blog/views.py
@@ -4,11 +4,13 @@ from taggit.models import Tag
 
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from decouple import config
 from django.views.decorators.http import require_POST
 
 from django.db.models import Count
+
+from django.contrib.postgres.search import SearchVector
 
 
 def post_list(request, tag_slug=None):
@@ -44,10 +46,10 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     form = CommentForm()
     post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).\
+    similar_posts = Post.published.filter(tags__in=post_tags_ids). \
         exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).\
-        order_by('-same_tags', '-publish')[:4]
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')). \
+                        order_by('-same_tags', '-publish')[:4]
     context = {
         'post': post,
         'comments': comments,
@@ -105,3 +107,21 @@ def post_comment(request, post_id):
         'comment': comment,
     }
     return render(request, 'post/comment.html', context)
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'), )\
+                .filter(search=query)
+    return render(request, 'post/search.html', {
+        'form': form,
+        'query': query,
+        'results': results,
+    })
